88859723b4f0c4317a931ca86f46086398265fe5
TomasPospisil97 <tp.tomaspospisil@gmail.com>
2024-05-23 21:48:58 +0200

    added physics of the game, gameover, text, score, etc.

--- function.js
+++ function.js
@@ -31,6 +31,12 @@ let bottomPipeImg;
 
 //fyzika hry
 let velocityX = -2; //trubky se posouvají doleva rychlostí
+let velocityY = 0; // rychlost skákání flappyho
+let gravity = 0.4; //gravitace flappyho
+let score = 0;
+
+//gameover
+let gameOver = false;
 
 window.onload = function (){
     playboard = document.getElementById("playboard");
@@ -58,25 +64,65 @@ window.onload = function (){
 
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); // 1.5sekundy
+    document.addEventListener("keydown", moveBird);
 
 }
-
+//update function - nejdulezitejsi funkce, na ktere hra funguje
 function update () {
     requestAnimationFrame(update);
+    if (gameOver) {
+        return; 
+    }
     context.clearRect(0, 0, playboard.width, playboard.height);
 
     //flappy
+    velocityY += gravity;
+    //flappy.y += velocityY;
+    flappy.y = Math.max(flappy.y + velocityY, 0); // make sure to not go higher that the screen
     context.drawImage(flappyImg, flappy.x, flappy.y, flappy.width, flappy.height);
+
+    if (flappy.y > playboard.height) {
+        gameOver = true;
+    }
 
     //pipes
     for (let i = 0; i < pipeArray.length; i++) {
         let pipe = pipeArray[i];
         pipe.x += velocityX;
         context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+
+        if (!pipe.passed && flappy.x > pipe.x + pipe.width) {
+            score += 0.5; //protoze jsou2 pipes, takze kdyz by zustalo +=1,tak se budou body pricitat po 2
+            pipe.passed = true;
+        }
+
+        if (detectCollision(flappy, pipe)) {
+            gameOver = true;
+        }
+    }
+
+    //clear the pipes that went "left" out of the screen (memory zatizeni), -pipe.width znamena, ze pipe zmizi s "pravym" rohem trubky takže jakoby "hezky"
+    while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+        pipeArray.shift(); //odstrani prvni element 
+    }
+
+    //score
+    context.fillStyle = "white";
+    context.font = "45px sans-serif";
+    context.fillText(score, 5, 45);
+
+    //text po skonceni hry
+    if (gameOver) {
+        context.fillText("GAME OVER", 40, 320);
+        context.fillText("Press X to", 80, 380);
+        context.fillText("continue", 100, 420);
     }
 }
 
 function placePipes () {
+    if (gameOver) {
+        return;
+    }
     //pipeArray.push(new Pipe(pipeX, pipeY, pipeWidth, pipeHeight, topPipeImg, bottomPipeImg));
 
     // (0-1) * pipeHeigth/2
@@ -107,5 +153,23 @@ function placePipes () {
     pipeArray.push(bottomPipe);
 
 }
+function moveBird(e) {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+        velocityY = -6; //jumping
 
-// 3007
+        //reset the game
+        if (gameOver) {
+            flappy.y = flappyY;
+            pipeArray = [];
+            score = 0;
+            gameOver = false;
+        }
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&
+           a.x + a.width > b.x &&
+           a.y < b.y + b.height &&
+           a.y + a.height > b.y;
+}
